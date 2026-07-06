b80efa27e724431d1a19f86c487e8ed68726fbd1
Sarty1234 <74873961+Sarty1234@users.noreply.github.com>
2026-07-06 15:32:39 +0300

    js injection: done callback on update, needs post request

--- jsinjection.js
+++ jsinjection.js
@@ -1,61 +1,58 @@
-
-//       Finding UI elements
-const songNameLabel = document.querySelector(`[data-testid="context-item-link"]`);
-const songAuthorLabel = document.querySelector(`[data-testid="context-item-info-artist"]`);
+const playerBox = document.querySelector(`[data-testid="now-playing-widget"]`);
 
 const nextSongButton = document.querySelector(`[data-testid="control-button-skip-forward"]`);
 const pauseButton = document.querySelector(`[data-testid="control-button-playpause"]`);
 
 
 function GetSongName() {
+    const songNameLabel = document.querySelector(`[data-testid="context-item-link"]`);
+    const songAuthorLabel = document.querySelector(`[data-testid="context-item-info-artist"]`);
+
     const name = songNameLabel?.textContent?.trim() || "Unknown Title";
     const artist = songAuthorLabel?.textContent?.trim() || "Unknown Artist";
-    const response = '${name}  ●  ${artist}'
+    const response = `${name}  ●  ${artist}`
     return response;
 }
 
 
-function OnSongChange() {
-    alert(GetSongName());
+function OnSongChange(newName) {
+    console.log(GetSongName());
 }
 
 
 let lastSong = "";
 const callback = (mutationsList, observer) => {
-    alert("stat");
     for (const mutation of mutationsList) {
         if (mutation.type === 'childList' || mutation.type === "characterData")
         {
             const currentSong = GetSongName();
 
-            if (currentSong === lastSong) {
-                continue;
+            if (currentSong !== lastSong) {
+                lastSong = currentSong;
+
+                let response = OnSongChange();
+
+                // if (response === "skipSong") {
+                //     nextSongButton.click();
+                // }
+                //  else if (response === "start") {
+                //     pauseButton.click();
+                // }
+                break;
             }
-            lastSong = currentSong;
-
-            let response = OnSongChange();
-
-            // if (response === "skipSong") {
-            //     nextSongButton.click();
-            // }
-            //  else if (response === "start") {
-            //     pauseButton.click();
-            // }
-            break;
         }
     }
 };
     
- // ///////////////////   do simple while loop 100 ms delay checking for update
+
 const observer = new MutationObserver(callback);
 
 // 4. Start observing the target element with specific configurations
-observer.observe(songNameLabel, {
+observer.observe(playerBox, {
         childList: true,  
         characterData: true,
         subtree: true
 });
-alert("stat");
 
 // To stop watching the element later:
 // observer.disconnect();

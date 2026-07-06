a5ae0353a30347cc9b72b1c8986980cb7ad1f756
Sarty1234 <74873961+Sarty1234@users.noreply.github.com>
2026-07-06 15:12:11 +0300

    Revert "while loop"
    This reverts commit 419b2471b161a46bd6d6091df2a4a625fada5043.

--- jsinjection.js
+++ jsinjection.js
@@ -10,50 +10,52 @@ const pauseButton = document.querySelector(`[data-testid="control-button-playpau
 function GetSongName() {
     const name = songNameLabel?.textContent?.trim() || "Unknown Title";
     const artist = songAuthorLabel?.textContent?.trim() || "Unknown Artist";
-    const response = `${name}  ●  ${artist}`;
-    console.log(response);
+    const response = '${name}  ●  ${artist}'
     return response;
 }
 
 
-function OnSongChange(newName) {
-    // alert(GetSongName());
-    console.log("!!!!!!!   New Song" + `${newName}`);
+function OnSongChange() {
+    alert(GetSongName());
 }
 
 
-function delay(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
-}
-
-
-
-8
 let lastSong = "";
-let updateID = 0;
-function Update() {
-    console.log("Update #" + `${updateID}`);
+const callback = (mutationsList, observer) => {
+    alert("stat");
+    for (const mutation of mutationsList) {
+        if (mutation.type === 'childList' || mutation.type === "characterData")
+        {
+            const currentSong = GetSongName();
 
-    const currentSong = GetSongName();
-    if (currentSong !== lastSong) 
-    {
-        lastSong = currentSong;
-        let response = OnSongChange(currentSong);
+            if (currentSong === lastSong) {
+                continue;
+            }
+            lastSong = currentSong;
+
+            let response = OnSongChange();
+
+            // if (response === "skipSong") {
+            //     nextSongButton.click();
+            // }
+            //  else if (response === "start") {
+            //     pauseButton.click();
+            // }
+            break;
+        }
     }
-
-
-    updateID++;
 };
+    
+ // ///////////////////   do simple while loop 100 ms delay checking for update
+const observer = new MutationObserver(callback);
 
+// 4. Start observing the target element with specific configurations
+observer.observe(songNameLabel, {
+        childList: true,  
+        characterData: true,
+        subtree: true
+});
+alert("stat");
 
-async function Start() 
-{
-    alert("start");
-    while (true) {
-        Update();
-        await delay(100);
-    }
-}
-
-
-Start();
+// To stop watching the element later:
+// observer.disconnect();

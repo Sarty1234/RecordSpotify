10d98e099ecd7eebc3b274256708d646e83c15f4
Sarty1234 <74873961+Sarty1234@users.noreply.github.com>
2026-07-07 23:48:52 +0300

    added sound recording

--- RecordSpotify/MainWindow.xaml.cs
+++ RecordSpotify/MainWindow.xaml.cs
@@ -23,43 +23,154 @@ namespace RecordSpotify
         public MainWindow()
         {
             InitializeComponent();
+
+            UpdateStatusLabel();
         }
 
+
+        enum RecordingStatus
+        {
+            Recording,
+            WaitingToStart,
+            WaitingToStop,
+            Stopped
+        }
+
+
+        private RecordingStatus recordingStatus = RecordingStatus.Stopped;
+        private void ToggleRecordingButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (recordingStatus == RecordingStatus.Recording)
+            {
+                recordingStatus = RecordingStatus.WaitingToStop;
+            }
+            else if (recordingStatus == RecordingStatus.WaitingToStart)
+            {
+                recordingStatus = RecordingStatus.Stopped;
+            }
+            else if (recordingStatus == RecordingStatus.WaitingToStop)
+            {
+                recordingStatus = RecordingStatus.WaitingToStart;
+            }
+            else if (recordingStatus == RecordingStatus.Stopped)
+            {
+                recordingStatus = RecordingStatus.WaitingToStart;
+            }
+
+            UpdateStatusLabel();
+        }
+
+        private void NewSongStarted(string songname)
+        {
+            CurrentSongLabel.Content = $"{songname}";
+
+            if (recordingStatus == RecordingStatus.Recording)
+            {
+                StopAudioCapture();
+                SaveFinishedRecordToDB();
+
+                if (IsFirstTimeRecordingThisSong(songname))
+                {
+                    StartAudioCapture(songname);
+                }
+                else
+                {
+                    // //////////////     send skip request
+                    recordingStatus = RecordingStatus.WaitingToStart;
+                }
+            }
+            else if (recordingStatus == RecordingStatus.WaitingToStart)
+            {
+                StartAudioCapture(songname);
+                recordingStatus = RecordingStatus.Recording;
+            }
+            else if (recordingStatus == RecordingStatus.WaitingToStop)
+            {
+                StopAudioCapture();
+                recordingStatus = RecordingStatus.Stopped;
+            }
+
+            UpdateStatusLabel();
+        }
+
+        private void UpdateStatusLabel()
+        {
+            if (recordingStatus == RecordingStatus.Recording)
+            {
+                StatusLabel.Content = "Status: Recording";
+            }
+            else if (recordingStatus == RecordingStatus.WaitingToStart)
+            {
+                StatusLabel.Content = "Status: Waiting next song to start";
+            }
+            else if (recordingStatus == RecordingStatus.WaitingToStop)
+            {
+                StatusLabel.Content = "Status: Waiting next song to stop";
+            }
+            else if (recordingStatus == RecordingStatus.Stopped)
+            {
+                StatusLabel.Content = "Status: Stopped";
+            }
+        }
+
+
+        private bool IsFirstTimeRecordingThisSong(string name)
+        {
+            // ///////////////// Add actual check
+            return true;
+        }
+
+        private void SaveFinishedRecordToDB()
+        {
+            // ///////////////// Add actual save
+        }
 
 
 
 
         private static WasapiLoopbackCapture _capture;
         private static LameMP3FileWriter _writer;
-        private static string _outputFilePath = "audio\\{filename}.mp3";
-
+        private static string _outputFilePath = "audio";
         private void StartAudioCapture(string filename)
         {
+            string songPath = _outputFilePath + $"\\{filename}.mp3";
+            Directory.CreateDirectory(_outputFilePath);
+
             _capture = new WasapiLoopbackCapture();
-            _writer = new LameMP3FileWriter(_outputFilePath.Replace("{filename}", filename), _capture.WaveFormat, 128);
+            _writer = new LameMP3FileWriter(songPath, _capture.WaveFormat, 128);
+
 
             _capture.DataAvailable += OnDataAvailable;
-            _capture.RecordingStopped += RecordingStopped;
 
             _capture.StartRecording();
         }
 
         private void StopAudioCapture()
         {
-            _capture.StopRecording();
+            if (_capture != null)
+            {
+                _capture.DataAvailable -= OnDataAvailable;
+
+                _capture.StopRecording();
+                _capture.Dispose();
+                _capture = null;
+            }
+
+            if (_writer != null)
+            {
+                _writer.Flush();
+                _writer.Dispose();
+                _writer = null;
+            }
         }
 
         private static void OnDataAvailable(object sender, WaveInEventArgs e)
         {
+            if (_writer == null) return;
+
             _writer.Write(e.Buffer, 0, e.BytesRecorded);
-            _writer.Flush();
         }
-        
-        private static void RecordingStopped(object sender, StoppedEventArgs e)
-        {
-            _writer?.Dispose();
-            _capture?.Dispose();
-        }
+
 
 
         const string InjectionFilePath = "jsinjection.js";
@@ -76,7 +187,6 @@ namespace RecordSpotify
 
             CopyInjectionButton.Content = temp;
             CopyInjectionButton.IsEnabled = true;
-
         }
     }
 }
